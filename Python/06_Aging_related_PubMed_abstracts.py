3978004f877ef5d9348c57685edb25578eb030a7
Søren Norge Andreassen <46787229+Sorennorge@users.noreply.github.com>
2019-05-28 12:19:55 +0200

    Update and rename 06_Selection_of_abstracts.py to 06_Aging_related_PubMed_abstracts.py

--- Python/06_Aging_related_PubMed_abstracts.py
+++ Python/06_Aging_related_PubMed_abstracts.py
@@ -5,7 +5,7 @@ Created on Fri Mar 22 18:33:45 2019
 @author: soren
 """
 
-### Selection of correct shared abstracts ###
+### Selection of aging-related (shared) abstracts ###
 
 ## import packages ##
 
@@ -14,11 +14,11 @@ import numpy as np
 
 ## Subdirectories ##
 
-Sub_in = "PubMed Search/Aging terms found in abstracts"
-Sub_in_2 = "PubMed Search/Aging synonyms found in abstracts"
+Sub_in = "PubMed Search/Clinical terms found in abstracts"
+Sub_in_2 = "PubMed Search/Aging keywords found in abstracts"
 
 Sub_out_main = "New terms mining"
-Sub_out = "New terms mining/PMID shared Aging terms and Synonyms"
+Sub_out = "New terms mining/PMID shared Clinical terms and Aging keywords"
 
 try:
     os.mkdir(Sub_out_main)
@@ -32,15 +32,15 @@ except Exception:
 
 ## Files ##
 
-File1 = "Aging_terms_found_in_abstracts_SNOMED_extension.txt"
-File2 = "Aging_synonyms_found_in_abstracts_SNOMED_extension.txt"
+File1 = "Clinical_terms_found_in_abstracts_SNOMED_extension.txt"
+File2 = "Aging_keywords_found_in_abstracts_SNOMED_extension.txt"
 
-File_out = "Combined_Aging_terms_and_synonyms_PMID_list.txt"
+File_out = "Combined_Clinical_terms_and_keywords_PMID_list.txt"
 
 ## Variables ##
 
-Aging_terms_two_plus = []
-Aging_synonyms = []
+Clinical_terms_two_plus = []
+Aging_keywords = []
 
 #Get all pmid for aging terms with 2 or more terms
 with open(os.path.join(Sub_in,File1), "r") as Aging_terms_file:
@@ -50,22 +50,22 @@ with open(os.path.join(Sub_in,File1), "r") as Aging_terms_file:
         #create a array to check if there is more than 1 term present in the abstract
         temp = np.array(line[1:],dtype=int)
         if np.sum(temp) >= 2:
-            Aging_terms_two_plus.append(str(line[0]))
+            Clinical_terms_two_plus.append(str(line[0]))
         else:
             pass
 Aging_terms_file.close
 
 #load all abstract with aging synonyms
-with open(os.path.join(Sub_in_2,File2), "r") as Aging_synonyms_file:
-    next(Aging_synonyms_file)
-    for line in Aging_synonyms_file:
+with open(os.path.join(Sub_in_2,File2), "r") as Aging_keywords_file:
+    next(Aging_keywords_file)
+    for line in Aging_keywords_file:
         line = line.strip().split(';')
-        Aging_synonyms.append(str(line[0]))
-Aging_synonyms_file.close
+        Aging_keywords.append(str(line[0]))
+Aging_keywords_file.close
 
 #Compare the lists and combined shared entries into a list
 
-Combined_list = list(set(Aging_terms_two_plus) & set(Aging_synonyms))
+Combined_list = list(set(Clinical_terms_two_plus) & set(Aging_keywords))
 
 with open(os.path.join(Sub_out,File_out), "w+") as Combined_list_file:
     for key in Combined_list:
